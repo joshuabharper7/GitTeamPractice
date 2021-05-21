fe00ac1dfb85dd006c4cd1d282fa99e67a7a74af
joshuabharper7 <84295436+joshuabharper7@users.noreply.github.com>
2021-05-21 14:31:06 -0400

    Initial commit

---
+++ GitTeamPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTeamPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
